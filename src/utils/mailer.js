0ea59e758ab22215e1b91ace0e8326001b0dd6b5
vishwajith <vishwakr1998@gmail.com>
2026-05-30 10:29:59 +0530

    removed unwanted codes

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,15 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-
-// module.exports = transporter;
-
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
